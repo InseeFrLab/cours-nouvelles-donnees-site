a708d0dfcac5c2ac23d2937993d45adbbdc56662
lgaliana <lino.galiana@insee.fr>
2025-01-08 19:42:42 +0000

    snapshot renv

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install python3-dev
+sudo apt-get install -y \
+    libudunits2-dev \
+    libgdal-dev \
+    libgeos-dev \
+    libproj-dev \
+    libmysqlclient-dev
