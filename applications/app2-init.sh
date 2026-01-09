08d2f117787c1b2b2f915ee19ae9589c11e0d734
jpramil <julien.pramil@insee.fr>
2026-01-09 16:43:17 +0000

    up app2

--- applications/app2-init.sh
+++ applications/app2-init.sh
@@ -8,6 +8,7 @@ echo "data/" >> .gitignore
 
 curl https://minio.lab.sspcloud.fr/projet-formation/diffusion/mlops/data/firm_activity_data.parquet --output data/data.parquet
 curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/naf2008_liste_n5.xls --output data/naf.parquet
+curl -O https://raw.githubusercontent.com/InseeFrLab/cours-nouvelles-donnees-site/main/applications/processor.py
 
 pip install wordcloud
 pip install xlrd
