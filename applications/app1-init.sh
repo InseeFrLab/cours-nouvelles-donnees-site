9b5e22b36ebd7f3bb764dbf042afe15817474088
Nicolas Toulemonde <192305627+ntoulemonde@users.noreply.github.com>
2026-01-09 18:41:02 +0100

    Small fix app1 (#6)
    * with git commands in app1 init script
    
    * update app1 with init script
    
    * with intro sentence to launch sspcloud service
    
    * small updates
    
    * ajout autres ex

--- applications/app1-init.sh
+++ applications/app1-init.sh
@@ -3,9 +3,14 @@
 mkdir appli1
 cd appli1
 
+# Initializing git repo and renaming branch to main
+git init
+git branch -m main
+
 mkdir data
 echo "data/" >> .gitignore
 
+# Download data from S3
 curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/geoparquet/dvf.parquet --output data/dvf.parquet
 curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/geoparquet/carreaux.parquet --output data/carreaux.parquet
 curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/triangle.geojson --output data/triangle.geojson
