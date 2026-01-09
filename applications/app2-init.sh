1fd39081ebe2046ce908062df821c6886079fe6c
jpramil <julien.pramil@insee.fr>
2026-01-09 16:16:02 +0000

    up app2

--- applications/app2-init.sh
+++ applications/app2-init.sh
@@ -6,7 +6,7 @@ cd ape
 mkdir data
 echo "data/" >> .gitignore
 
-curl ttps://minio.lab.sspcloud.fr/projet-formation/diffusion/mlops/data/firm_activity_data.parquet --output data/data.parquet
+curl https://minio.lab.sspcloud.fr/projet-formation/diffusion/mlops/data/firm_activity_data.parquet --output data/data.parquet
 curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/naf2008_liste_n5.xls --output data/naf.parquet
 
 pip install wordcloud
