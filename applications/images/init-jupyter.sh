b8f8eeb4b1e17a255132d419f3ad46edd613ee27
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2026-01-12 16:39:34 +0000

    update apps images with kernel using uv

--- applications/images/init-jupyter.sh
+++ applications/images/init-jupyter.sh
@@ -4,10 +4,10 @@ CLONE_DIR="${WORK_DIR}/cours-nouvelles-donnees-site"
 
 # Clone course repository
 REPO_URL="https://github.com/inseefrlab/cours-nouvelles-donnees-site/"
-git clone --depth 1 $REPO_URL $CLONE_DIR
+git clone $REPO_URL $CLONE_DIR --branch dev_nt  # Remove dev_nt when pushed to main
 
 # Copy relevant notebooks to work directory
-cp ${CLONE_DIR}/applications/images/{classification_oiseau.ipynb,donnees_satellite.ipynb, pyproject.toml, uv.lock} ${WORK_DIR}
+cp ${CLONE_DIR}/applications/images/{classification_oiseau.ipynb,donnees_satellite.ipynb,pyproject.toml,uv.lock} ${WORK_DIR}
 
 # Remove repo
 rm -rf "${CLONE_DIR}"
@@ -15,21 +15,10 @@ rm -rf "${CLONE_DIR}"
 # Creating a Jupyter Kernel with all dependancies pre installed
 # https://docs.astral.sh/uv/guides/integration/jupyter/#using-jupyter-within-a-project
 
-# Create and activate virtual environment
-uv venv .venv
-source .venv/bin/activate
+# Install ipykernel and create a Jupyter kernel with dependencies open
+uv add --dev ipykernel
+uv run ipython kernel install --user --env VIRTUAL_ENV $(pwd)/.venv --name=application_images
 
-# Install ipykernel and create a Jupyter kernel
-uv add -dev ipykernel
-uv run ipython kernel install --user --env VIRTUAL_ENV $(pwd)/.venv --name=project
-
-# Open the first notebook when starting Jupyter Lab
-# Generate Jupyter config if it doesn't exist
-if [ ! -f /home/onyxia/.jupyter/jupyter_server_config.py ]; then
-    uv run jupyter server --generate-config -y
-fi
-echo "c.LabApp.default_url = '/lab/tree/classification_oiseau.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
-
-# Run Jupyter Lab
-uv run --with jupyter jupyter lab
+# Delete config files
+rm pyproject.toml uv.lock
 
