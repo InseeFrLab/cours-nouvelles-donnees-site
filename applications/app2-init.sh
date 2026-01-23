a73206537a6e7f5b4aa481719290e5263a94508a
Nicolas Toulemonde <192305627+ntoulemonde@users.noreply.github.com>
2026-01-23 17:16:48 +0100

    Update pip with uv and renv with rv - except app 1 and app 2 (#17)
    * uv init
    
    * with pip setuptools wheels and updated test.yml
    
    * with rv
    
    * with rv in GHA
    
    * curl with retry number
    
    * fix naf file extension
    
    * update ape init script branch to main
    
    * with rv and uv installation
    
    * with bash
    
    * with uv action
    
    * with max time to 3 min
    
    * with rv action version complete

--- applications/app2-init.sh
+++ applications/app2-init.sh
@@ -6,9 +6,9 @@ cd ape
 mkdir data
 echo "data/" >> .gitignore
 
-curl https://minio.lab.sspcloud.fr/projet-formation/diffusion/mlops/data/firm_activity_data.parquet --output data/data.parquet
-curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/naf2008_liste_n5.xls --output data/naf.parquet
-curl -O https://raw.githubusercontent.com/InseeFrLab/cours-nouvelles-donnees-site/main/applications/processor.py
+curl https://minio.lab.sspcloud.fr/projet-formation/diffusion/mlops/data/firm_activity_data.parquet --output data/data.parquet  --retry 5 --retry-all-errors --max-time 300
+curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/naf2008_liste_n5.xls --output data/naf.xls  --retry 5 --retry-all-errors --max-time 300
+curl -O https://raw.githubusercontent.com/InseeFrLab/cours-nouvelles-donnees-site/main/applications/processor.py  --retry 5 --retry-all-errors --max-time 300
 
 pip install wordcloud
 pip install xlrd
--- preview_site.sh
+++ preview_site.sh
@@ -1,6 +1,6 @@
 # cd cours-nouvelles-donnees-site/
-# git checkout maj-25 
-pip install -r requirements.txt
-Rscript -e "renv::restore()"
-bash download_nlp_reqs.sh
-quarto preview
+# git checkout maj-25
+uv sync
+rv sync
+uv run bash download_nlp_reqs.sh
+uv run quarto preview
