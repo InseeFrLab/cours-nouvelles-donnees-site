1c4dc9cbb1e02bc54075fbe7fdcb90427a20f5cd
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2026-01-23 14:04:10 +0000

    change retry number and max time

--- applications/app1-init.sh
+++ applications/app1-init.sh
@@ -6,8 +6,8 @@ cd appli1
 mkdir data
 
 # Download data from S3
-curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/geoparquet/dvf.parquet --output data/dvf.parquet --retry 3 --retry-all-errors --max-time 5
-curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/geoparquet/carreaux.parquet --output data/carreaux.parquet --retry 3 --retry-all-errors --max-time 5
-curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/triangle.geojson --output data/triangle.geojson --retry 3 --retry-all-errors --max-time 5
-curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/malakoff.geojson --output data/malakoff.geojson --retry 3 --retry-all-errors --max-time 5
-curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/montrouge.geojson --output data/montrouge.geojson --retry 3 --retry-all-errors --max-time 5
+curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/geoparquet/dvf.parquet --output data/dvf.parquet --retry 5 --retry-all-errors --max-time 15
+curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/geoparquet/carreaux.parquet --output data/carreaux.parquet --retry 5 --retry-all-errors --max-time 15
+curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/triangle.geojson --output data/triangle.geojson --retry 5 --retry-all-errors --max-time 15
+curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/malakoff.geojson --output data/malakoff.geojson --retry 5 --retry-all-errors --max-time 15
+curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/montrouge.geojson --output data/montrouge.geojson --retry 5 --retry-all-errors --max-time 15
--- download_nlp_reqs.sh
+++ download_nlp_reqs.sh
@@ -9,12 +9,12 @@ python -m spacy download fr_core_news_sm
 # App APE
 mkdir applications/data
 
-curl https://minio.lab.sspcloud.fr/projet-formation/diffusion/mlops/data/firm_activity_data.parquet --output applications/data/data.parquet   --retry 3 --retry-all-errors --max-time 5
-curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/naf2008_liste_n5.xls --output applications/data/naf.xls  --retry 3 --retry-all-errors --max-time 5
+curl https://minio.lab.sspcloud.fr/projet-formation/diffusion/mlops/data/firm_activity_data.parquet --output applications/data/data.parquet   --retry 5 --retry-all-errors --max-time 15
+curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/naf2008_liste_n5.xls --output applications/data/naf.xls  --retry 5 --retry-all-errors --max-time 15
 
 mkdir applications/model_ape
 
-curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/model_ape/metadata.pkl --output applications/model_ape/metadata.pkl  --retry 3 --retry-all-errors --max-time 5
-curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/model_ape/model_checkpoint.ckpt --output applications/model_ape/model_checkpoint.ckpt  --retry 3 --retry-all-errors --max-time 5
-curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/model_ape/tokenizer.pkl --output applications/model_ape/tokenizer.pkl  --retry 3 --retry-all-errors --max-time 5
+curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/model_ape/metadata.pkl --output applications/model_ape/metadata.pkl  --retry 5 --retry-all-errors --max-time 15
+curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/model_ape/model_checkpoint.ckpt --output applications/model_ape/model_checkpoint.ckpt  --retry 5 --retry-all-errors --max-time 15
+curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/model_ape/tokenizer.pkl --output applications/model_ape/tokenizer.pkl  --retry 5 --retry-all-errors --max-time 15
 
