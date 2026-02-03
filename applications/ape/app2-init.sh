59cc85604568d87b7e5492ab0476bbb73b811599
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2026-02-03 18:47:29 +0000

    output processor

--- applications/ape/app2-init.sh
+++ applications/ape/app2-init.sh
@@ -8,7 +8,7 @@ echo "data/" >> .gitignore
 
 curl https://minio.lab.sspcloud.fr/projet-formation/diffusion/mlops/data/firm_activity_data.parquet --output data/data.parquet  --retry 5 --retry-all-errors --max-time 300
 curl https://minio.lab.sspcloud.fr/projet-formation/nouvelles-sources/data/naf2008_liste_n5.xls --output data/naf.xls  --retry 5 --retry-all-errors --max-time 300
-curl -O https://raw.githubusercontent.com/InseeFrLab/cours-nouvelles-donnees-site/main/applications/processor.py  --retry 5 --retry-all-errors --max-time 300
+curl -O https://raw.githubusercontent.com/InseeFrLab/cours-nouvelles-donnees-site/main/applications/ape/processor.py  --output ape/processor.py --retry 5 --retry-all-errors --max-time 300
 
 pip install wordcloud
 pip install xlrd
@@ -19,4 +19,4 @@ pip install pytorch_lightning
 pip install torchTextClassifiers[huggingface]
 pip install scikit-learn
 
-echo -e "# Script du TP\n # Ci-dessous une balise pour voir les résultats en interactif\n# %%" > script_tp.py
+echo -e "# Script du TP\n# Ci-dessous une balise pour voir les résultats en interactif\n# %%" > script_tp.py
